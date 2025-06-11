10df00caf7e96c12d10805873eac646e282005e8
lassc123 <3096079594@qq.com>
2025-06-11 08:58:02 +0800

    新增timer源码

--- 25_6_10/main.cpp
+++ 25_6_10/main.cpp
@@ -22,20 +22,22 @@ int main() {
 }
 
 //源码：
-class timer {
-public:
-    //std::clock()
-    timer() {_start_time = std::clock();}
-    void restart() {_start_time = std::clock();}
-    double elapsed() const{
-        return (std::clock() - _start_time)/static_cast<double>(CLOCKS_PER_SEC);
-    }
-    double elapsed_min() const {
-        return (1)/static_cast<double>(CLOCKS_PER_SEC);
-    }
-    double elapsed_max() const {
-        return (static_cast<double>((std::numeric_limits<std::clock_t>::max)()) - static_cast<double>(_start_time)/static_cast<double>(CLOCKS_PER_SEC));
-    }
-private:
-    std::clock_t _start_time;
-};
+// class timer {
+// public:
+//     //std::clock()函数，返回自进程启动以来的clokc数
+//     timer() {_start_time = std::clock();}//以当前的clock为计时起点
+//     void restart() {_start_time = std::clock();}//更新clokc数，进行重新计时
+//     double elapsed() const{
+//         return (std::clock() - _start_time)/static_cast<double>(CLOCKS_PER_SEC)//每秒的clock数
+//     }
+//     double elapsed_min() const {
+//         return (1)/static_cast<double>(CLOCKS_PER_SEC);//timer能测量的最小时间
+//     }
+//     double elapsed_max() const {
+//         return (static_cast<double>((std::numeric_limits<std::clock_t>::max)()) - static_cast<double>(_start_time)/static_cast<double>(CLOCKS_PER_SEC));
+//         利用标准库的数值极限类numeric_limits来获得clokc_t的最大值，从而求得能测量的最大时间范围
+//     }
+//     无析构函数->仅有一个类型为clokc_t的成员变量_start_time,不需要析构函数来特意释放申请的资源
+// private:
+//     std::clock_t _start_time;
+// };
